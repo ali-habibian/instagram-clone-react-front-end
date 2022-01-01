5250cd0732cedcf5bfb446919d1eca9bd905c7a3
Ali Habibian <ali.habibian04@gmail.com>
2022-01-01 16:49:31 +0330

    implemented sign in component

--- src/Components/LoginPage/LoginPage.js
+++ src/Components/LoginPage/LoginPage.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-import '../LoginPage/LoginPage.css';
+import "../LoginPage/LoginPage.css";
 import { Grid } from "@material-ui/core";
 import inst_image from "../../images/9364675fb26a.svg";
 import insta_logo from "../../images/logoinsta.png";
+import fb_icon from "../../images/fb.png";
+import appstore_icon from "../../images/app.png";
+import googlestore_icon from "../../images/play.png";
+import SignIn from "../Signin/SignIn";
 
 class LoginPage extends Component {
   constructor(props) {
@@ -15,7 +19,7 @@ class LoginPage extends Component {
       <div>
         <Grid container>
           <Grid item xs={3}>
-            aaa
+
           </Grid>
           <Grid item xs={6}>
             <div className="loginpage_main">
@@ -24,14 +28,46 @@ class LoginPage extends Component {
               </div>
               <div>
                 <div className="loginpage_rightcomponent">
-                  <img className="loginpage_logo" src={insta_logo} alt="insta logo" />
+                  <img
+                    className="loginpage_logo"
+                    src={insta_logo}
+                    alt="insta logo"
+                  />
+                  <div className="loginpage_signin">
+                    <SignIn/>
+
+                    <div className="loginpage_ordiv">
+                      <div className="loginpage_divder"></div>
+                      <div className="loginpage_or">OR</div>
+                      <div className="loginpage_divder"></div>
+                    </div>
+
+                    <div className="loginpage_facebook"><img src={fb_icon} width="15px" alt="fb_icon" style={{ "marginRight": "5px" }} /> Log in with Facebook</div>
+                    <div className="loginpage_forgot">Forgot password?</div>
+                  </div>
                 </div>
+
+                <div className="loginpage_singupoption">
+                  <div className="loginpage_singup">
+                    Don't have an account? <span style={{"fontWeight":"bold", "color":"#0395F6"}}>sign up</span>
+                  </div>
+                  <div className="loginpage_singin">
+                    have an account? <span style={{"fontWeight":"bold", "color":"#0395F6"}}>sign in</span>
+                  </div>
+                </div>
+
+                <div className="loginpage_downloadsection">
+                  <div className="loginpage_downloadsection_text">Get the app.</div>
+                  <div className="loginpage_downloadsection_option">
+                    <img className="loginpage_downloadsection_img" src={appstore_icon} width="136px" alt="appstore_icon"/>
+                    <img  className="loginpage_downloadsection_img" src={googlestore_icon} width="136px" alt="googlestore_icon"/>
+                  </div>
+                </div>
+
               </div>
             </div>
           </Grid>
-          <Grid item xs={3}>
-            
-          </Grid>
+          <Grid item xs={3}></Grid>
         </Grid>
       </div>
     );
