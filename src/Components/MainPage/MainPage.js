df943b0ff64f3cb8f50972e5abdd07c9af834928
Ali Habibian <ali.habibian04@gmail.com>
2022-01-01 18:19:05 +0330

    added StatusBar and MainPage components

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../MainPage/MainPage.css';
+
+class MainPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>Post</div>
+         );
+    }
+}
+ 
+export default MainPage;
---
+++ src/Components/StatusBar/StatusBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../StatusBar/StatusBar.css';
+
+class StatusBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>StatusBar</div>
+         );
+    }
+}
+
+export default StatusBar;
