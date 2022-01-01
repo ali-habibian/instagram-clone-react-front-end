67e63ef6cf3b23f1c4a6cfb1abb83316ccf17cab
Ali Habibian <ali.habibian04@gmail.com>
2022-01-02 03:24:59 +0330

    implemented info and suggestions component

--- src/Components/InfoSection/InfoSection.js
+++ src/Components/InfoSection/InfoSection.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import '../InfoSection/InfoSection.css';
+import { Avatar } from "@material-ui/core";
+import profileImage from '../../images/pp1.png';
 
 class InfoSection extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
-            <div>InfoSection</div>
-         );
+    render() {
+        return (
+            <div className='info_container'>
+                <Avatar className='info_profileImage' src={profileImage} />
+                <div className='info_content'>
+                    <div className='info_userName'>User_name</div>
+                    <div className='info_description'>Description</div>
+                </div>
+            </div>
+        );
     }
 }
- 
+
 export default InfoSection;
--- src/Components/Suggestions/Suggestions.js
+++ src/Components/Suggestions/Suggestions.js
@@ -1,20 +1,52 @@
 import React, { Component } from 'react';
 import '../Suggestions/Suggestions.css';
+import { Avatar } from "@material-ui/core";
+import avatarImage from '../../images/pp1.png';
 
 class Suggestions extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
-        return ( 
+    render() {
+        return (
             <div>
-                Suggestions
+                <div className='suggestions_container'>
+                    <div className='suggestions_header'>
+                        <div>Suggestion For You</div>
+                    </div>
+                    <div className='suggestions_body'>
+                        <div className='suggestions_friends'>
+                            <Avatar className='suggestions_prfileImage' src={avatarImage} alt='avatarImage' />
+                            <div className='suggestions_userName'>Friend 1</div>
+                        </div>
+
+                        <div className='suggestions_friends'>
+                            <Avatar className='suggestions_prfileImage' src={avatarImage} alt='avatarImage' />
+                            <div className='suggestions_userName'>Friend 2</div>
+                        </div>
+
+                        <div className='suggestions_friends'>
+                            <Avatar className='suggestions_prfileImage' src={avatarImage} alt='avatarImage' />
+                            <div className='suggestions_userName'>Friend 3</div>
+                        </div>
+
+                        <div className='suggestions_friends'>
+                            <Avatar className='suggestions_prfileImage' src={avatarImage} alt='avatarImage' />
+                            <div className='suggestions_userName'>Friend 4</div>
+                        </div>
+
+                        <div className='suggestions_friends'>
+                            <Avatar className='suggestions_prfileImage' src={avatarImage} alt='avatarImage' />
+                            <div className='suggestions_userName'>Friend 5</div>
+                        </div>
+                    </div>
+                </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default Suggestions;
 <div>
     Suggestions
